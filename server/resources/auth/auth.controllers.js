f7677accafa8a9d5dee7ff8db66715df039c6d48
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-03-28 13:08:59 +0100

    added data, and necessary files

---
+++ server/resources/auth/auth.controllers.js
@@ -0,0 +1,65 @@
+const fetchUsers = require("../../utils/fetchUsers");
+const fs = require("fs").promises;
+const bcrypt = require("bcrypt");
+
+const register = async (req, res) => {
+  //Innehåller användarens data, mail och lösenord.
+  const { email, password } = req.body;
+
+  //Kolla så användare inte redan finns
+  const users = await fetchUsers();
+  const userAlreadyExists = users.find((u) => u.email === email);
+
+  if (userAlreadyExists) {
+    return res.status(400).json("User already exists");
+  }
+
+  //Kryptera lösenord
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //Spara till databasen
+  const newUser = {
+    email,
+    password: hashedPassword,
+  };
+  users.push(newUser);
+  await fs.writeFile("./data/users.json", JSON.stringify(users, null, 2));
+
+  //Skicka tillbaka ett svar
+  res.status(201).json(newUser.email);
+};
+
+const login = async (req, res) => {
+  //Kolla så användaren finns (mest troligt validering med joi också sen)
+  const { email, password } = req.body;
+
+  const users = await fetchUsers();
+  const userExists = users.find((u) => u.email === email);
+
+  //Kolla så lösenordet stämmer
+
+  if (!userExists || !(await bcrypt.compare(password, userExists.password))) {
+    return res.status(400).json("Wrong user or password");
+  }
+
+  //Logga in anvädaren, starta en session
+  req.session.user = userExists;
+
+  //Skicka tillbaka ett svar
+  res.status(200).json("Logged in " + userExists.email);
+};
+
+const logout = (req, res) => {
+  req.session = null; //Rensa och döda sessionen.
+  res.status(200).json("Logged out");
+};
+
+const authorize = (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json("You are not logged in");
+  }
+  //Denna man ska kunna knyta till isAuthenticated.
+  res.status(200).json(req.session.user.email);
+};
+
+module.exports = { register, login, logout, authorize };
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cookieSession = require("cookie-session");
+const cors = require("cors");
+
+const userRouter = require("./resources/users/users.router");
+const authRouter = require("./resources/auth/auth.router");
+
+const app = express();
+//Vi behöver tala om för cors vad som tillåts.
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+//Inställningarna för cookies
+app.use(
+  cookieSession({
+    // keys: ["jdsijdi", "jadjs"] Man kan ha antingen key eller secret
+    secret: "s3cr3tk3y",
+    maxAge: 1000 * 60 * 60, // 1 h - man kan också använda expires.
+    //httpOnly: true, -Säkerhet, kan inte nås via javascript, bara i http-protokollet, true by defualt dock.
+    // secure: false  - också by default.
+    // sameSite: "lax", - ocså default om none tillåts 3partscookies då måste secure vara true.
+  })
+);
+
+//Routes
+//Denna tar oss till router som tar oss vidare till controllers där getUser-funktionen finns.
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
+app.listen(3001, () => console.log("server is up and running.. "));
