aaa143c03b7274a7d625531fed601af6072df80c
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-04-09 12:34:34 +0200

    added cart modal and removed hardcoded items

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,6 +7,8 @@ import "../styles/header.css";
 import { Login } from "./Login";
 import { Logout } from "./Logout";
 import { Register } from "./Register";
+import { useState } from "react";
+import { Cart } from "./Cart";
 
 interface IHeaderProps {
   user: string;
@@ -14,6 +16,11 @@ interface IHeaderProps {
 
 export const Header = ({ user }: IHeaderProps) => {
   const { cart } = useCart();
+  const [openCart, setOpenCart] = useState<boolean>(false);
+
+  const handleOpenCart = () => {
+    setOpenCart(!openCart);
+  };
 
   return (
     <>
@@ -28,11 +35,29 @@ export const Header = ({ user }: IHeaderProps) => {
         <h1>
           Some<span>Webshop</span>
         </h1>
-        <div className="cart">
+        <div className="cart" onClick={handleOpenCart}>
           <FiShoppingCart />
           <div className="cart-count">{cart.length}</div>
         </div>
       </div>
+
+      <Cart openCart={openCart} handleOpenCart={handleOpenCart} />
+
+      {/* <Modal
+        open={openCart}
+        onClose={handleOpenCart}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <div style={{ backgroundColor: "white", padding: 20 }}>
+          {cart.map((item, index) => (
+            <div key={index}>
+              <h4>{item.product.name}</h4>
+              <p>Price: {item.product.price}</p>
+              <p>Quantity: {item.quantity}</p>
+            </div>
+          ))}
+        </div>
+      </Modal> */}
     </>
   );
 };
