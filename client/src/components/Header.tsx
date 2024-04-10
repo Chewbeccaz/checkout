da853a1baa93df05c52af6d879ec3eaa79a87a25
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-04-10 15:19:01 +0200

    added some styling, moved cart to navigation, etc

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,46 +1,51 @@
 import { useCart } from "../context/CartContext";
-import { FiShoppingCart } from "react-icons/fi";
+
 import "../styles/header.css";
 import { Register } from "./Register";
 import { useState } from "react";
 import { Cart } from "./Cart";
 import Navigation from "./Navigation";
 import { useUser } from "../context/UserContext";
-import { Login } from "./Login";
-import { Logout } from "./Logout";
+// import { Login } from "./Login";
+// import { Logout } from "./Logout";
 
 export const Header = () => {
-  const { cart } = useCart();
+  // const { cart } = useCart();
   const { user } = useUser();
-  const [openCart, setOpenCart] = useState<boolean>(false);
+  // const [openCart, setOpenCart] = useState<boolean>(false);
 
-  const handleOpenCart = () => {
-    setOpenCart(!openCart);
-  };
+  // const handleOpenCart = () => {
+  //   setOpenCart(!openCart);
+  // };
 
   return (
     <>
-      <Navigation />
-      <div>
-        <h1>{user ? "INLOGGAD: " + user.email : "UTLOGGAD"}</h1>
-        <Login />
-        <Logout />
-      </div>
-      <div>{!user && <Register />}</div>
-
       <div className="header">
+        <div className="logga">
+          <h3>WEBSHOP</h3>
+        </div>
+        <div className="navigation">
+          <Navigation />
+        </div>
+        <div>
+          <h1>{user ? "INLOGGAD: " + user?.email : "UTLOGGAD"}</h1>
+          {/* <Login />
+        <Logout /> */}
+        </div>
+        {/* <div>{!user && <Register />}</div> */}
+
         <h1>
           Some<span>Webshop</span>
         </h1>
-        {user && (
+        {/* {user && (
           <div className="cart" onClick={handleOpenCart}>
             <FiShoppingCart />
             <div className="cart-count">{cart.length}</div>
           </div>
-        )}
+        )} */}
       </div>
 
-      {user && <Cart openCart={openCart} handleOpenCart={handleOpenCart} />}
+      {/* {user && <Cart openCart={openCart} handleOpenCart={handleOpenCart} />} */}
     </>
   );
 };
--- client/src/components/WebshopApp.tsx
+++ client/src/components/WebshopApp.tsx
@@ -1,7 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { Register } from "./Register";
-// import { Login } from "./Login";
-// import { Logout } from "./Logout";
 import { Products } from "./Products";
 import "../styles/webshopapp.css";
 import CartProvider from "../context/CartContext";
@@ -9,47 +5,6 @@ import { Header } from "./Header";
 import { UserProvider } from "../context/UserContext";
 
 export const WebshopApp = () => {
-  // const [user, setUser] = useState<string>("");
-  //Behöver jag ha denna??
-  //Dessa två gör ingen skillnad.
-  // useEffect(() => {
-  //   const authorize = async () => {
-  //     const response = await fetch("http://localhost:3001/api/auth/authorize", {
-  //       credentials: "include",
-  //     });
-  //     const data = await response.json();
-  //     if (response.status === 200) {
-  //       setUser(data);
-  //     } else {
-  //       setUser("");
-  //     }
-  //   };
-  //   authorize();
-  // }, []);
-  // const { setLoggedInUser } = useUser();
-  // useEffect(() => {
-  //   const authorize = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "http://localhost:3001/api/auth/authorize",
-  //         {
-  //           credentials: "include",
-  //         }
-  //       );
-  //       if (response.status === 200) {
-  //         const data = await response.json();
-  //         setLoggedInUser(data);
-  //       } else {
-  //         setLoggedInUser(undefined);
-  //       }
-  //     } catch (error) {
-  //       console.error("Authorization error:", error);
-  //     }
-  //   };
-
-  //   authorize();
-  // }, []);
-
   return (
     <UserProvider>
       <CartProvider>
